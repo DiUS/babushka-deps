89b54e4278311f3694b55267f4a940470c7f58f9
azzamallow <azzamallow@gmail.com>
2013-01-30 13:28:20 +1100

    updated example

--- aaron_example.rb
+++ aaron_example.rb
@@ -1,9 +1,8 @@
 dep 'llvm in path' do
-    requires 'xcode tools'
-      met? { which 'llvm-gcc-4.2' }
-        meet {
-              cd('/usr/local/bin') {|path|
-                shell "ln -s /Developer/usr/llvm-gcc-4.2/bin/llvm* .", :sudo => !path.writable?
-                    }
-                      }
+  met? { which 'llvm-gcc-4.2' }
+  meet {
+    cd('/usr/local/bin') {|path|
+      shell "ln -s /Developer/usr/llvm-gcc-4.2/bin/llvm* .", :sudo => !path.writable?
+    }
+  }
 end
