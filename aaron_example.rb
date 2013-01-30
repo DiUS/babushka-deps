e8b42f4ee3835cedbb8f82f53a994d93c66d09fa
azzamallow <azzamallow@gmail.com>
2013-01-30 14:10:53 +1100

    updated

--- aaron_example.rb
+++ aaron_example.rb
@@ -1,8 +1,3 @@
-dep 'aarons_stuff' do
-  met? { which 'llvm-gcc-4.2' }
-  meet {
-    cd('/usr/local/bin') {|path|
-      shell "ln -s /Developer/usr/llvm-gcc-4.2/bin/llvm* .", :sudo => !path.writable?
-    }
-  }
+dep 'jenkins.bin' do
+  installs 'jenkins'
 end
