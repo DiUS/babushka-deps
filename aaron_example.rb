8cc1fd1c33edbcf1fc5bcae416a2962197611529
azzamallow <azzamallow@gmail.com>
2013-01-30 12:21:21 +1100

    test babushka dep

---
+++ aaron_example.rb
@@ -0,0 +1,9 @@
+dep 'llvm in path', :for => :snow_leopard do
+    requires 'xcode tools'
+      met? { which 'llvm-gcc-4.2' }
+        meet {
+              cd('/usr/local/bin') {|path|
+                shell "ln -s /Developer/usr/llvm-gcc-4.2/bin/llvm* .", :sudo => !path.writable?
+                    }
+                      }
+end
